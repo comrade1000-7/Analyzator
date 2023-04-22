a5f5d38e3d1b20bcee1039ec906fae164e59ce3e
Vasily Parshentsev <vparshentsev@gmail.com>
2023-04-22 17:44:26 +0300

    extract method getThread

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,11 @@
 import java.util.Random;
-import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.*;
 
 public class Main {
     public static ArrayBlockingQueue<String> charA = new ArrayBlockingQueue<>(100);
     public static ArrayBlockingQueue<String> charB = new ArrayBlockingQueue<>(100);
     public static ArrayBlockingQueue<String> charC = new ArrayBlockingQueue<>(100);
-    public static String maxStringA;
-    public static String maxStringB;
-    public static String maxStringC;
-    public static int lastResultA;
-    public static int lastResultB;
-    public static int lastResultC;
+
     public static String generateText(String letters, int length) {
         Random random = new Random();
         StringBuilder text = new StringBuilder();
@@ -20,18 +15,33 @@ public class Main {
         return text.toString();
     }
 
-    public static int maxCountChar (String request, char symbol) {
-        char[] arr = request.toCharArray();
-        int counter = 0;
-        for (char sym : arr) {
-            if (sym == symbol) {
-                counter++;
+    public static Thread getThread(BlockingQueue<String> queue, char letter) {
+        return new Thread(() -> {
+            try {
+                int max = maxCountChar(queue, letter);
+                System.out.println("В потоке " + Thread.currentThread() + " больше всего " + letter + ": " + max);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-        }
-        return counter;
+        });
     }
 
-    public static void main(String[] args) throws InterruptedException {
+    public static int maxCountChar(BlockingQueue<String> queue, char symbol) throws InterruptedException {
+        int count = 0;
+        int max = 0;
+        String temp;
+        for (int i = 0; i < 10_000; i++) {
+            temp = queue.take();
+            for (char a : temp.toCharArray()) {
+                if (a == symbol) count++;
+            }
+            if (count > max) max = count;
+            count = 0;
+        }
+        return max;
+    }
+
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         Thread producer = new Thread(() -> {
             for (int i = 0; i < 10_000; i++) {
                 try {
@@ -46,50 +56,9 @@ public class Main {
         });
         producer.start();
 
-        Thread threadA = new Thread(() -> {
-            while (producer.isAlive()) {
-                try {
-                    String temp = charA.take();
-                    int actualResult = maxCountChar(temp, 'a');
-                    if (actualResult > lastResultA) {
-                        lastResultA = actualResult;
-                        maxStringA = temp;
-                    }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-
-        Thread threadB = new Thread(() -> {
-            while (producer.isAlive()) {
-                try {
-                    String temp = charB.take();
-                    int actualResult = maxCountChar(temp, 'b');
-                    if (actualResult > lastResultB) {
-                        lastResultB = actualResult;
-                        maxStringB = temp;
-                    }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
-
-        Thread threadC = new Thread(() -> {
-            while (producer.isAlive()) {
-                try {
-                    String temp = charC.take();
-                    int actualResult = maxCountChar(temp, 'c');
-                    if (actualResult > lastResultC) {
-                        lastResultC = actualResult;
-                        maxStringC = temp;
-                    }
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
-                }
-            }
-        });
+        Thread threadA = getThread(charA, 'a');
+        Thread threadB = getThread(charB, 'b');
+        Thread threadC = getThread(charC, 'c');
 
         threadA.start();
         threadB.start();
@@ -104,10 +73,5 @@ public class Main {
         threadA.interrupt();
         threadB.interrupt();
         threadC.interrupt();
-
-
-        System.out.println("Больше всего а " + maxStringA);
-        System.out.println("Больше всего b " + maxStringB);
-        System.out.println("Больше всего c " + maxStringC);
     }
 }
